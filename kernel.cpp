1dea62f900d589ccb5611c8d3cd64e0e2820f301
Akiicat <aaaa1379@gmail.com>
2019-03-22 15:42:24 +0800

    add comment to kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,9 +1,18 @@
 
+// 將 string 寫到特定的記憶體位置 0xb8000
+// 顯示卡則會去此位置抓取值，將文字 render 到螢幕上
+//
+// 以 16 個 bit 為單位組成一個文字：
+//   - high byte:
+//     - 4bit: front ground
+//     - 4bit: back ground
+//   - low byte:
+//     - 8bit: char
 void printf(char *str) {
 	unsigned short * VideoMemory = (unsigned short *) 0xb8000;
 
 	for (int i = 0; str[i] != '\0'; ++i) {
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) |str[i];
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 	}
 }
 
