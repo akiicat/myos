f7d5bdfba205f5abb376b65ef35c50aa9058d15d
Akiicat <aaaa1379@gmail.com>
2019-03-25 10:02:55 +0800

    fix intend in kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -13,9 +13,9 @@
 void printf(char *str) {
   static uint16_t * VideoMemory = (uint16_t *) 0xb8000;
 
-	for (int i = 0; str[i] != '\0'; ++i) {
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-	}
+  for (int i = 0; str[i] != '\0'; ++i) {
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+  }
 }
 
 // Write a custom contructor
@@ -30,7 +30,7 @@ extern "C" void callConstructors() {
 
 
 extern "C" void kernelMain(void *multiboot_structure, uint16_t magicnumber) {
-	printf("Hello World!");
+  printf("Hello World!");
 
-	while(1);
+  while(1);
 }
