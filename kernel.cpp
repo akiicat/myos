784e1880908e89f3916af5b2cfd813ce58cef39f
Akiicat <aaaa1379@gmail.com>
2019-03-28 13:30:32 +0800

    add change new line feature and fix the print data do not leave in the same line

--- kernel.cpp
+++ kernel.cpp
@@ -11,11 +11,43 @@
 //     - 4bit: back ground
 //   - low byte:
 //     - 8bit: char
+//
+// screen = 25 x 80 char
 void printf(char *str) {
   static uint16_t * VideoMemory = (uint16_t *) 0xb8000;
 
+  static uint8_t x = 0, y = 0;
+
   for (int i = 0; str[i] != '\0'; ++i) {
-    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
+    switch (str[i]) {
+      case '\n':
+        y++;
+        x = 0;
+        break;
+      default:
+        VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+        x++;
+    }
+
+
+    if (x >= 80) {
+      y++;
+      x = 0;
+    }
+
+    if (y >= 25) {
+      for (y = 0; y < 25; y++) {
+        for (x = 0; x < 80; x++) {
+          VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+          x++;
+        }
+      }
+
+      x = 0;
+      y = 0;
+    }
+
   }
 }
 
