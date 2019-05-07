717e33cfeec2d647e589ac23217c1926c2b38cb4
Akiicat <aaaa1379@gmail.com>
2019-05-07 19:25:01 +0800

    add lscpi usage comment

--- include/hardwarecommunication/pci.h
+++ include/hardwarecommunication/pci.h
@@ -6,6 +6,20 @@
 #include <hardwarecommunication/port.h>
 #include <hardwarecommunication/interrupts.h>
 
+/* 
+ * $ lspci -n
+ * 00:00.0 0600: 8086:29a0 (rev 02)
+ *               <vender_id>:<device_id>
+ *
+ * $ lspci -x
+ * 00:00.0 Host bridge: Intel Corporation 82P965/G965 Memory Controller Hub (rev 02)
+ * 00: 86 80 a0 29 06 01 90 00 02 00 00 06 00 00 00 00
+ * 10: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+ * 20: 00 00 00 00 00 00 00 00 00 00 00 00 b8 1a 00 04
+ * 30: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+ *
+ */
+
 namespace myos {
 
   namespace hardwarecommunication {
